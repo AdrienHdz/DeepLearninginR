84b2cf8a63d192e7531b71d62047335db9c6287a
Estefan <brownchilean@gmail.com>
2020-03-30 21:46:47 -0400

    Added FNN comments

--- FFNN.R
+++ FFNN.R
@@ -21,13 +21,47 @@ print(tail(Y$train))
 #############################
 ############ KERAS
 #############################
+
+library(datasets)
+# Download the Iris data to workspace
+data(iris)
+# Change the data Iris output for string 
+iris$Species = sapply(as.character(iris$Species), 
+                      switch, "setosa" = 1, 
+                      "versicolor" = 2, 
+                      "virginica" = 3, 
+                      USE.NAMES = F)
+# Here is another way, to split the data to 
+spec = c(train = .7,test = .3)
+# Set a seed in order to be repreductible
+set.seed()
+# Sample through the dataframe using the sample and cut.
+# The variable "g" returns a list of rows for train and
+# test
+g = sample(cut(seq(nrow(iris)),
+               nrow(iris)*cumsum(c(0,spec)),
+               labels = names(spec)
+)
+)
+# Use the data and the row information to select rows
+data_df = split(iris, g)
+# Create vector that will contain X (Features variable)
+# and Y the target variable
+X = c()
+Y = c()
+X$train = as.matrix(data_df$train[,-ncol(data_df$train)])
+X$test = as.matrix(data_df$test[,-ncol(data_df$test)])
+Y$train = as.matrix(data_df$train[,ncol(data_df$train)])
+Y$test = as.matrix(data_df$test[,ncol(data_df$test)],)
+
 library(keras)
 
 # create model
 model_keras = keras_model_sequential()
 model_keras %>% 
-  layer_dense(units = 8, activation = 'relu', input_shape = c(4)) %>% 
-  layer_dense(units = 3, activation = 'softmax')
+  layer_dense(units = 8, activation = 'relu', input_shape = c(3)) %>% 
+  layer_dense(units = 4, activation = 'softmax')
+summary(model_keras)
 # Compile model
 model_keras %>% compile(
   loss = 'categorical_crossentropy',
@@ -39,6 +73,8 @@ history <- model_keras %>% fit(
   as.matrix(X$train),to_categorical(Y$train), 
    epochs=200, batch_size=5)
 end_time <- Sys.time()
+
+plot(history)
 
 #Benchmark
 time_fnn_keras = end_time - start_time
@@ -115,7 +151,36 @@ summary(model_mx)
 pred.label.mxnet <- max.col(t(preds)) - 1
 1-mean(pred.label.mxnet != as.vector(data_df$test$Species))
 
+#############################
+############ Neuralnet
+#############################
+install.packages("neuralnet")
+library(neuralnet)
 
+install.packages("datasets")
+library(datasets)
+n=nrow(iris)
+size.train=floor(n*0.7); size.test=floor(n*0.3)
+
+set.seed(123)
+# Definition of the observations ID assigned to the train and test data
+id.train=sample(1:n,size.train,replace=FALSE)
+id.test=sample(setdiff(1:n,id.train),size.test,replace=FALSE)
+# We create the training and test dataset
+iris_train=iris[id.train,]; iris_test=iris[id.test,]
+start_time <- Sys.time()
+neuralnet_model <- neuralnet((Species=="setosa") +
+                               (Species=="versicolor") +
+                               (Species=="virginica") ~
+                               Sepal.Length+Sepal.Width+
+                               Petal.Length+Petal.Width,
+                             rep = 1, data = iris_train,
+                             algorithm = "backprop",
+                             learningrate = 0.01,
+                             linear.output = FALSE, hidden = c(3, 3),
+                             stepmax = 1000000, act.fct = "logistic")
+end_time <- Sys.time()
+time_fnn_neuralnet = end_time - start_time
 ############# SAVE WORKSPACE
 
 save.image("C:/Users/apabl/Desktop/Git/DeepLearninginR/DeepLearninginR/FNN_save.RData")
